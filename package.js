6ccc9fe8715d9e19e9f5ecefa3a2667cb8d1b0ba
Abigail Watson <awatson1978@gmail.com>
2015-02-08 22:55:55 -0500

    need to export Dictionary

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   summary: "Meteorite package that provides a the Webster Unabridged English Dictionary.",
 
-    // update this value before you run 'meteor publish'
-    version: "1.0.2",
+  // update this value before you run 'meteor publish'
+  version: "1.0.3",
 
-    // if this value isn't set, meteor will default to the directory name
-    name: "awatson1978:dataset-dictionary",
+  // if this value isn't set, meteor will default to the directory name
+  name: "awatson1978:dataset-dictionary",
 
-    // and add this value if you want people to access your code from Atmosphere
-    git: "http://github.com/awatson1978/dataset-dictionary.git"
+  // and add this value if you want people to access your code from Atmosphere
+  git: "http://github.com/awatson1978/dataset-dictionary.git"
 });
 
 Package.on_use(function (api) {
-    api.export('Dictionary');
-    api.use('standard-app-packages@1.0.4');
-    api.addFiles('initialize.dictionary.js', ["client","server"]);
+  api.export('Dictionary');
+  api.use('standard-app-packages@1.0.4');
+  api.addFiles('initialize.dictionary.js', ["client","server"]);
 });
